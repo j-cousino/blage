212fbd31451fac3b7fb80f2da90c4e89396d909c
Jeff Cousino <jeff.cousino@soluforge.com>
2024-12-04 06:21:28 -0500

    baseline

--- src/lib.rs
+++ src/lib.rs
@@ -3,26 +3,9 @@
 //! [items.person]
 //! name = "String"
 
-use serde::{Serialize,Deserialize};
-
-
-#[derive(Serialize, Deserialize)]
-pub struct ItemPropery {
-    toml_type: String,
-    required: bool,
-}
-
-
-impl ItemPropery {
-    pub fn new(name: &str, toml_type: &str, required: bool) -> ItemPropery {
-
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 
 }
